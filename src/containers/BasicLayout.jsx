385afeb86266a00e091eb08844b1247d5b545e03
wangqiong <2550145203@qq.com>
2019-10-18 22:45:02 +0800

    1、添加首页 页面布局修改

--- src/containers/BasicLayout.jsx
+++ src/containers/BasicLayout.jsx
@@ -61,8 +61,12 @@ class BasicLayout extends React.Component {
               <Link to='/transshipment'>转载</Link>
             </Menu.Item>
             <Menu.Item key="6">
-              <Link to='/openSource'>开源项目</Link>
+              <Link to='/openSource'>开源</Link>
             </Menu.Item>
+            <Menu.Item key="7">
+              <Link to='/about'>关于</Link>
+            </Menu.Item>
+
             <SubMenu
               title={
                 <span className="submenu-title-wrapper">
@@ -70,9 +74,7 @@ class BasicLayout extends React.Component {
                 </span>
               }
             >
-              <Menu.Item key="7">
-                <Link to='/about'>关于</Link>
-              </Menu.Item>
+              <Menu.Item key="52">建议</Menu.Item>
               <Menu.Item key="53">工具</Menu.Item>
             </SubMenu>
           </Menu>
@@ -81,54 +83,65 @@ class BasicLayout extends React.Component {
             onSearch={value => console.log(value)}
             className="search"
           />
+          <i className="iconfont icon-user admin" />
         </Header>
 
         <Content className="basic-layout-content">
-          <div style={{ margin: '0 auto', width: 740 }}>
-            <Switch>
-              <Route
-                extra
-                key='1'
-                path={'/home'}
-                component={Home}
-              />
-              <Route
-                extra
-                key='2'
-                path={'/category'}
-                component={Category}
-              />
-              <Route
-                extra
-                key='3'
-                path={'/label'}
-                component={Label}
-              />
-              <Route
-                extra
-                key='4'
-                path={'/archive'}
-                component={Archive}
-              />
-              <Route
-                extra
-                key='5'
-                path={'/transshipment'}
-                component={Transshipment}
-              />
-              <Route
-                extra
-                key='6'
-                path={'/openSource'}
-                component={OpenSource}
-              />
-              <Route
-                extra
-                key='7'
-                path={'/about'}
-                component={About}
-              />
-            </Switch>
+          <div className='content'>
+            <div className='left'>
+              <div className='card'>
+                <Switch>
+                  <Route
+                    extra
+                    key='1'
+                    path={'/home'}
+                    component={Home}
+                  />
+                  <Route
+                    extra
+                    key='2'
+                    path={'/category'}
+                    component={Category}
+                  />
+                  <Route
+                    extra
+                    key='3'
+                    path={'/label'}
+                    component={Label}
+                  />
+                  <Route
+                    extra
+                    key='4'
+                    path={'/archive'}
+                    component={Archive}
+                  />
+                  <Route
+                    extra
+                    key='5'
+                    path={'/transshipment'}
+                    component={Transshipment}
+                  />
+                  <Route
+                    extra
+                    key='6'
+                    path={'/openSource'}
+                    component={OpenSource}
+                  />
+                  <Route
+                    extra
+                    key='7'
+                    path={'/about'}
+                    component={About}
+                  />
+                </Switch>
+              </div>
+              <div className='card'></div>
+            </div>
+            <div className='right'>
+              <div className='card'>11111</div>
+              <div className='card'>22222</div>
+              <div className='card'>33333</div>
+            </div>
           </div>
         </Content>
         <Footer className="basic-layout-footer">
